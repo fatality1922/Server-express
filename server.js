dcf850cfd1b807fe1aa47500e80a9793c81db176
Danciak <fatality1922@gmail.com>
2021-04-13 19:22:29 +0200

    add form feature

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -26,7 +27,7 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/history', (req, res, next) => {
-  res.render('history', {layout: 'dark'});
+  res.render('history', { layout: 'dark' });
 });
 
 
@@ -34,6 +35,19 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
 
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, userimage } = req.body;
+
+  if (author && sender && title && message && userimage) {
+    res.render('contact', { isSent: true, userimage });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
